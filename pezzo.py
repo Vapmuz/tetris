9e5d1c808fdab4d7c72277f68d1540aafa250104
broder <broder@>
2025-08-01 11:31:09 +0200

    fatto sistema rotazioni più test rotazioni

--- pezzo.py
+++ pezzo.py
@@ -9,4 +9,33 @@ class Pezzo:
 
     def __str__(self):
       return f'{self.name}:l={len(self.pos)}'
+    
+    
+    # data una tupla di cordinate 
+    # relative (x,y) genera una tupla ruotata di 90 gradi a destra
 
+    def rotpos(selfself,xy):  
+        x,y = xy
+        a=0
+        if x>0 and y==0:
+            return(y,-x)
+            a= a+1
+        if x==0 and y<0:
+            return(y,x)
+            a= a+1
+
+        if x<0 and y==0:
+            return(y,-x)
+            a= a+1
+
+        if x==0 and y>0:
+            return(y,x)
+            a= a+1
+
+
+
+
+    def rotate(self):
+        lRotpos= list(map(self.rotpos, self.pos ))
+        self.pos= lRotpos
+        return(self) 
--- pezzo_unittest.py
+++ pezzo_unittest.py
@@ -6,3 +6,14 @@ class TestPezzo(unittest.TestCase):
         p= Pezzo('punto','x',[(0,0)])
         #self.assertEqual(2,1)
         self.assertEqual('punto:l=1',str(p))
+
+    def test_rotpos(self):
+        p=Pezzo('vuoto','x', [(0,0)])
+        self.assertEqual( (0,1) , p.rotpos( (-1,0) ), 'rotazione tupla vuota' )
+        self.assertEqual( (-1,0) , p.rotpos( (0,-1) ), 'rotazione tupla vuota' )        
+        self.assertEqual( (0,-1) , p.rotpos( (1,0) ), 'rotazione tupla vuota' )       
+        self.assertEqual( (1,0) , p.rotpos( (0,1) ), 'rotazione tupla vuota' )
+
+    def test_rotate(self):
+        p= Pezzo('vuoto','x', [(0,1),(0,2)])
+        self.assertEqual([ (1,0) , (2,0) ],p.rotate().pos)
