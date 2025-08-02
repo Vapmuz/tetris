c00c95b38323468b6ee8dd0dbf00df5a861b7385
Lenz <lenz@home>
2025-08-02 14:23:05 +0200

    Aggiunto generatore di pezzi casuale

---
+++ nextup.py
@@ -0,0 +1,67 @@
+"""
+Restituisce il prossimo pezzo, nella sua posizione base.
+
+- The Random Generator (also known as "random bag" or "7 bag") determines the sequence of tetrominoes during gameplay. One of each of the 7 tetrominoes are shuffled in a "bag", and are dealt out one by one. When the bag is empty, a new one is filled and shuffled.
+- Tetrominoes appear on the 21st and 22nd rows of the playfield, centered and rounded to the left when needed. They must start with their flat side down, and move down immediately after appearing.
+
+"""
+import random
+from pezzo import Pezzo, ESSE, ELLE
+
+
+class Nextup:
+    """
+    La classe che implementa i prossimi pezzi, caricati una bag alla volta.
+    La classe tiene il conto del numero di pezzi prelevati.
+    """
+
+    def __init__(self):
+        """Genera un oggetto vuoto."""
+        self.queue = []
+        self.pops = 0
+
+    def __str__(self):
+        """Genera: 'Nextup:jlfr(56)' dove
+        il valore stringa sono i tipi di pezzi nella coda
+        e 56 il numero di pezzi prelevati tramite pop().
+        """
+        pezzi = ""
+        for p in self.queue:
+            pezzi += p.name
+        return f"Nextup:{pezzi}({self.pops})"
+
+    def fill(self):
+        """Aggiunge una bag di elementi alla mia coda."""
+        bag = [ESSE, ELLE]
+        random.shuffle(bag)
+        for p in bag:
+            self.queue.append(p)
+
+    def pop(self):
+        """
+        Restituisce il primo elemento dalla coda e lo 
+        elimina. 
+
+        Se non vi sono pezzi nella coda, ne aggiunge una bag.
+        """
+        if len(self.queue) == 0:
+            self.fill()
+
+        v = self.queue.pop(0)
+        self.pops += 1
+        return v
+
+    def peek(self, offset):
+        """
+        Mi restituisce il pezzo alla posizione 'offset' che 
+        verrà prelevato in futuro. Il pezzo NON viene pelevato,
+        quindi il pezzo '0' è il prossimo, il pezzo '1' quello successivo, 
+        e così via.
+
+        Se non vi sono abbastanza pezzi nella coda, li genera fino 
+        all'offset richiesto, aggiungendo un certo numero di bags.
+        """
+        # anche se l'offset è zero, devo fare un refill!
+        while len(self.queue) <= offset:
+            self.fill()
+        return self.queue[offset]
---
+++ nextup_unittest.py
@@ -0,0 +1,62 @@
+"""unit test"""
+
+import unittest
+from nextup import Nextup
+from pezzo import Pezzo
+
+
+class TestNextup(unittest.TestCase):
+    """Prova la bag per nextup"""
+
+    def test_creazione(self):
+        """Creo la classe"""
+        n = Nextup()
+        self.assertEqual("Nextup:(0)", str(n))
+
+    def test_peek_pop(self):
+        """Prova peek e pop"""
+        n = Nextup()
+        peeked = []
+        for p in range(100):
+            peeked.append(n.peek(p))
+        self.assertTrue(len(n.queue) >= 100,
+                        "ho fatto 100 peek quindi ho almeno 100 elementi")
+
+        for p in range(100):
+            v = n.pop()
+            self.assertEqual(v, peeked[p], f"Elemento in pos #{p}")
+
+        self.assertTrue(len(n.queue) < 10,
+                        "ho fatto 100 pop, tolto molti elementi")
+
+    def test_pop_autorefill(self):
+        """Controlla il refill automatico"""
+        n = Nextup()
+
+        for _ in range(100):
+            v = n.pop()
+            self.assertTrue(isinstance(v, Pezzo),
+                            f"pezzo {str(v)} è un Pezzo")
+
+    def test_conta_elementi(self):
+        """Conta il numero di elementi poppati"""
+        n = Nextup()
+        for _ in range(100):
+            n.pop()
+        self.assertEqual(100, n.pops)
+
+    def test_elementi_unici(self):
+        """Sui primi 7 elementi, ho 7 elementi unici"""
+        n = Nextup()
+        n_uniques = 2
+        items = []
+        for _ in range(n_uniques):
+            items.append(n.pop())
+        unique_items = set(map(lambda p: p.name, items))
+        self.assertEqual(n_uniques, len(unique_items))
+
+        # controllo siano sempre 7
+        for _ in range(107):
+            items.append(n.pop())
+        unique_items = set(map(lambda p: p.name, items))
+        self.assertEqual(n_uniques, len(unique_items))
